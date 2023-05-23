ae449f289ab1062e210b24440262a9febe267737
Admin <stas.revutsky@gmail.com>
2023-05-23 21:00:17 +0300

    Add project files.

---
+++ h1solution1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace h1solution1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int calcutePerimeter(int a)
+            {
+                int sidesOfSquare = 4;
+                int p = sidesOfSquare * a;
+                return p;
+            }
+            int calculateArea(int a)
+            {
+                return (int)Math.Pow(a, 2);
+            }
+
+            Console.WriteLine(calcutePerimeter(a) + " " + calculateArea(a));
+
+        }
+    }
+}
